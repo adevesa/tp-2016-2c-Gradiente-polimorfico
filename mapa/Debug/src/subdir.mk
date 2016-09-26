5e2cedc8e5e0b1af153eeda138344113287107a3
Ezequiel Escobar <ezequieloscarescobar2@gmail.com>
2016-09-25 23:09:30 -0300

    configuracion mapa y entrenador
    Signed-off-by: Ezequiel Escobar <ezequieloscarescobar2@gmail.com>

--- mapa/Debug/src/subdir.mk
+++ mapa/Debug/src/subdir.mk
@@ -32,7 +32,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/workspace/necessary-commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
