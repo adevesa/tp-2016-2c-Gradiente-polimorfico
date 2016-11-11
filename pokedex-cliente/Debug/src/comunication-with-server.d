decb5919eb203f0fb209100e6a38b91d0e5c7fc9
Ezequiel Escobar <ezequieloscarescobar2@gmail.com>
2016-11-10 22:10:27 -0300

    Configs de cliente
    Signed-off-by: Ezequiel Escobar <ezequieloscarescobar2@gmail.com>

---
+++ pokedex-cliente/Debug/src/comunication-with-server.d
@@ -0,0 +1,18 @@
+src/comunication-with-server.d: ../src/comunication-with-server.c \
+ ../src/comunication-with-server.h ../src/vars.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/serializacion_pokedex_cliente_servidor.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/../so-commons/string.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/sockets-utils.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/so-commons/log.h
+
+../src/comunication-with-server.h:
+
+../src/vars.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/serializacion_pokedex_cliente_servidor.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/../so-commons/string.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/sockets-utils.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/so-commons/log.h:
---
+++ pokedex-cliente/Debug/src/podekex-cliente.d
@@ -0,0 +1,23 @@
+src/podekex-cliente.d: ../src/podekex-cliente.c ../src/fuse-basic.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/so-commons/string.h \
+ ../src/comunication-with-server.h ../src/vars.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/serializacion_pokedex_cliente_servidor.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/../so-commons/string.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/sockets-utils.h \
+ /home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/so-commons/log.h
+
+../src/fuse-basic.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/so-commons/string.h:
+
+../src/comunication-with-server.h:
+
+../src/vars.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/serializacion_pokedex_cliente_servidor.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/../so-commons/string.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/socket/sockets-utils.h:
+
+/home/utnso/workspace/tp-2016-2c-Gradiente-polimorfico/necessary-commons/so-commons/log.h:
